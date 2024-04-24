dc7b9e967e64dd5d6d9584f6a0019e756b65fc53
Mykyta Yeremenko <nikr3enox@gmail.com>
2024-04-24 22:14:23 +0300

    clg firebase config
    clg firebase config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -9,5 +9,12 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+console.log('apiKey', firebaseConfig.apiKey);
+console.log('authDomain', firebaseConfig.authDomain);
+console.log('databaseURL', firebaseConfig.databaseURL);
+console.log('projectId', firebaseConfig.projectId);
+console.log('storageBucket', firebaseConfig.storageBucket);
+console.log('messagingSenderId', firebaseConfig.messagingSenderId);
+console.log('appId', firebaseConfig.appId);
 
 export const app = initializeApp(firebaseConfig);
